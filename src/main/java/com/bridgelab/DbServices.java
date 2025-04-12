5960615555567501dddf1a703b3b899ea6a1c92c
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-12 14:08:36 +0530

    [Deepak]add: getAllEmployee method to retrieve all employee data from the databse

--- src/main/java/com/bridgelab/DbServices.java
+++ src/main/java/com/bridgelab/DbServices.java
@@ -1,8 +1,9 @@
 package com.bridgelab;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DbServices {
 
@@ -13,11 +14,46 @@ public class DbServices {
         return  DriverManager.getConnection(jdbcUrl, username, password) ;
     }
 
-    
 
 
+    private  List<EmployeePayroll> mapSetToObject(ResultSet rs) throws SQLException {
+        List<EmployeePayroll> list = new ArrayList<>();
+
+        while (rs.next()) {
+
+            LocalDate date = rs.getDate("start_date").toLocalDate();
+
+            list.add(new EmployeePayroll(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("gender"),
+                    rs.getDouble("salary"), date));
+        }
+        return list;
+    }
 
 
+    public List<EmployeePayroll> getAllEmployee(){
+
+        List<EmployeePayroll> emplist = new ArrayList<>();
+
+        String query = "select * from employee_payroll";
+
+        try{
+            Connection conn= connection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            emplist = mapSetToObject(rs);
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return emplist;
+
+    }
 
 
 
--- src/main/java/com/bridgelab/Main.java
+++ src/main/java/com/bridgelab/Main.java
@@ -1,18 +1,27 @@
 package com.bridgelab;
 
+import java.util.List;
+
 public class Main {
+
+    public static void printlist(List<EmployeePayroll> list) {
+        for (EmployeePayroll x : list) {
+            System.out.println(x);
+        }
+    }
 
     public static void main(String[] args) {
 
 
         try {
-
-            DbServices services = new DbServices();
-
+            // set the connection
             DbServices.connection();
 
+            DbServices db = new DbServices();
 
-
+            // get all employee payroll
+            List<EmployeePayroll> employees = db.getAllEmployee();
+            printlist(employees);
 
 
         } catch (Exception e) {
